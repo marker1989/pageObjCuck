971c13bd5d86574e08d40f1774c7dca1a21f4db6
henio <malkelek@gmail.com>
2017-06-24 13:21:37 +0200

    zmiana adresu IP na docker

--- src/test/java/pl/laita/setup/Init.java
+++ src/test/java/pl/laita/setup/Init.java
@@ -1,19 +1,39 @@
 package pl.laita.setup;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class Init {
 	
+	//static FirefoxDriver driver
+	static WebDriver driver;
 	
-	static FirefoxDriver driver;
-	
-	public static FirefoxDriver getDriver(){
+	public static WebDriver getDriver(){
 		
 		if(driver ==null){
-			driver = new FirefoxDriver();
+			
+			
+			URL hubUr1 = null;
+			
+			try{
+				hubUr1 = new URL("http://192.168.0.119:4444/wd/hub/");
+			}catch (MalformedURLException e){
+				
+			}
+			
+			DesiredCapabilities cap = DesiredCapabilities.firefox();
+			
+			//driver = new FirefoxDriver();
+			driver = new RemoteWebDriver(hubUr1, cap);
+			//driver = new FirefoxDriver();
 			driver.get("http://newtours.demoaut.com");
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			return driver;
